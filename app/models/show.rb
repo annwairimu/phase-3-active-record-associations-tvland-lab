ef57fa93a38e254756513c8f1556db9b53ed50e2
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-06-01 21:40:06 +0300

    done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,19 @@
 class Show < ActiveRecord::Base
+        belongs_to (:network)
+        has_many (:characters)
+        has_many (:actors, through: :characters)
+
   
-end
+    def characters
+      Character.where(show_id: id)
+    end
+  
+    def network
+      Network.find(network_id)
+    end
+  
+    def actors_list
+      actors.map(&:full_name)
+    end
+  end
+  
